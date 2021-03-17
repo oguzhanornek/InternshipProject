fb386040914ddf99cefacc9845e30378924acb58
oguzhanornek <Oguz1907*>
2021-03-17 22:11:18 +0300

    passing data navigation id

--- app/src/main/java/com/oguzornek/internshipproject/adapter/KullaniciListesiRecyclerAdapter.kt
+++ app/src/main/java/com/oguzornek/internshipproject/adapter/KullaniciListesiRecyclerAdapter.kt
@@ -1,6 +1,5 @@
 package com.oguzornek.internshipproject.adapter
 
-import android.app.Notification
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,23 +7,32 @@ import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.oguzornek.internshipproject.R
 import com.oguzornek.internshipproject.model.Kullanicilar
-import com.oguzornek.internshipproject.view.KullanicilarFragment
 import com.oguzornek.internshipproject.view.KullanicilarFragmentDirections
 import kotlinx.android.synthetic.main.kullanici_recyler_row.view.*
 
 
-class KullaniciListesiRecyclerAdapter(var kullaniciListesi : List<Kullanicilar>) : RecyclerView.Adapter<KullaniciListesiRecyclerAdapter.KullaniciViewHolder>() {
+class KullaniciListesiRecyclerAdapter(var kullaniciListesi: List<Kullanicilar>) : RecyclerView.Adapter<KullaniciListesiRecyclerAdapter.KullaniciViewHolder>() {
 
+    class KullaniciViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
 
+        fun bind(item: Kullanicilar) {
+            itemView.kullaniciAdiText.text = item.kullaniciAdi
+            itemView.isimText.text = item.isim
+            itemView.mailText.text = item.kullaniciMail
 
-    class KullaniciViewHolder(itemview : View) : RecyclerView.ViewHolder(itemview){
+            itemView.setOnClickListener {
+                val userId: Int?
+                userId = item.userId
+                val action = KullanicilarFragmentDirections.actionKullanicilarFragmentToUserChooseFragment(userId!!)
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KullaniciViewHolder {
-
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.kullanici_recyler_row,parent,false)
+        val view = inflater.inflate(R.layout.kullanici_recyler_row, parent, false)
         return KullaniciViewHolder(view)
     }
 
@@ -33,19 +41,6 @@ class KullaniciListesiRecyclerAdapter(var kullaniciListesi : List<Kullanicilar>)
     }
 
     override fun onBindViewHolder(holder: KullaniciViewHolder, position: Int) {
-
-        val userId = kullaniciListesi.get(position).userId
-        holder.itemView.kullaniciAdiText.text = kullaniciListesi.get(position).kullaniciAdi
-        holder.itemView.isimText.text = kullaniciListesi.get(position).isim
-        holder.itemView.mailText.text = kullaniciListesi.get(position).kullaniciMail
-
-
-        holder.itemView.setOnClickListener(){
-
-            val action = KullanicilarFragmentDirections.actionKullanicilarFragmentToUserChooseFragment()
-            Navigation.findNavController(it).navigate(action)
-
-
-        }
+        holder.bind(kullaniciListesi.get(position))
     }
 }
--- app/src/main/java/com/oguzornek/internshipproject/view/UserChooseFragment.kt
+++ app/src/main/java/com/oguzornek/internshipproject/view/UserChooseFragment.kt
@@ -1,16 +1,20 @@
 package com.oguzornek.internshipproject.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.oguzornek.internshipproject.R
 import kotlinx.android.synthetic.main.fragment_user_choose.view.*
 
 
 class UserChooseFragment : Fragment() {
+    private val args : UserChooseFragmentArgs by navArgs()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +31,8 @@ class UserChooseFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Log.v("UserIdLog",args.userId.toString())
 
         view.postButton.setOnClickListener(){
             val action2 = UserChooseFragmentDirections.actionUserChooseFragmentToPostFragment()
